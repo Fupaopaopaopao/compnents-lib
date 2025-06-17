3f8bbc610089622b1e87fe523d5ed4b2d46ae8cd
Mac <fupaopao8483@gmail.com>
2025-06-17 15:57:48 +1000

    dropdown

---
+++ src/app/components/dropdown/index.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import React, { useEffect, useRef, useState } from 'react'
+import { motion, AnimatePresence } from "framer-motion";
+
+type DropdownProps ={
+    trigger : 'click' | 'hover';
+    children: React.ReactNode;
+    items:DropdownItem[];
+}
+type DropdownItem = {
+    label:React.ReactNode;
+    click:()=>void;
+}
+
+
+export const CustomDropDown = (props: DropdownProps) => {
+    const [showDrop,setShowDrop] = useState(false);
+
+const handleClick=()=>{
+    showDrop ? setShowDrop(false) : setShowDrop(true);
+}
+let timeoutId: NodeJS.Timeout;
+const handleMouseEnter =()=>{
+    clearTimeout(timeoutId);
+    setShowDrop(true);
+}
+const handleMouseLeave = ()=>{
+    timeoutId=setTimeout(() => setShowDrop(false), 400);
+}
+
+  
+  return (
+    <div className='relative' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div>{props.children}</div>
+        <AnimatePresence>
+            {showDrop &&(
+            <motion.div className='absolute mt-2 rounded shadow-lg bg-white border border-slate-200 p-2 z-50'
+               initial={{ scale: 0.9, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0.9, opacity: 0 }}
+            transition={{ duration: 0.2 }}> 
+                {props.items.map((item)=>(
+                    
+                    <div className='transition ease-in-out rounded hover:bg-slate-300 py-1 px-3' onClick={()=>{
+                        item.click();
+                        setShowDrop(false);
+                    }}>
+                        {item.label}
+                    </div>
+                ))}
+            </motion.div>
+        )}
+        </AnimatePresence>
+
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { CustomCard } from "./components/card";
 import { EditOutlined } from "@ant-design/icons";
 import { CustomModal } from "./components/modal";
 import { useState } from "react";
+import { CustomDropDown } from "./components/dropdown";
+import { p } from "framer-motion/client";
 
 export default function Home() {
   const texts = "Maybe we can live without libraries, people like you and me. Maybe. Sure, we're too old to change the world, but what about that kid, sitting down, opening a book, right now, in a branch at the local library and finding drawings of pee-pees and wee-wees on the Cat in the Hat and the Five Chinese Brothers? Doesn't HE deserve better?"
@@ -16,12 +18,34 @@ export default function Home() {
   const handleCancel =()=>{
     setShow(false);
   }
+  const clickItem1 = ()=>{
+    setShow(true);
+  }
+
+const items = [
+    {
+  
+    label: (
+      <p>item 1</p>
+    ),
+    click:clickItem1
+  },
+  {
+    
+    label: (
+      <p>item 2</p>
+    ),
+  click:clickItem1}
+]
 
 
   return (
     <div>
-      <div className="p-5">
+      <div className="flex p-5 gap-2">
         <CustomButton label="Modal" icon={<EditOutlined />} onClick={handleClick}/>
+        <CustomDropDown trigger="click" items={items}>
+          <CustomButton label="Dropdown" icon={<EditOutlined />}/>
+          </CustomDropDown>
       </div>
       <div className="p-5 flex flex-wrap gap-2">
         <CustomCard title="Title" subtitle="Subtitle" paragraph={texts} img="/images/test.png"/>
