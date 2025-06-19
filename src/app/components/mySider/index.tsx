b52b3a85861a33e97d03a0cfcfbbb4568dca536f
Mac <fupaopao8483@gmail.com>
2025-06-20 04:32:02 +1000

    modify click function

--- src/app/components/mySider/index.tsx
+++ src/app/components/mySider/index.tsx
@@ -6,11 +6,11 @@ import { label } from "framer-motion/client";
 
 export const SiderWrapper = () => {
   const clickitem1 = () => console.log(111);
-  const clickitem2 = () => console.log(222);
+  const clickitem2 = () => console.log("烦死了");
 
   const items: MenuItem[] = [
     { label: <p>ITEM 1 MENUITEM</p>, click: clickitem1, key: "item1" },
-    { label: <p>ITEM 2 MENUITEM</p>, click: clickitem2, key: "item2",children:[{label:<p>ITEM 2 SUBMENUITEM</p>,click:clickitem1,key:"subitem1"}] },
+    { label: <p>ITEM 2 MENUITEM</p>, click: clickitem2, key: "item2",children:[{label:<p>ITEM 2 SUBMENUITEM</p>,click:clickitem2,key:"subitem1"}] },
   ];
 
   return <Sider items={items} />;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,7 +51,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <CustomHeader/>
-        <SiderWrapper/>
+ 
+    <SiderWrapper/>
+
+        
 
         {children}
       </body>
